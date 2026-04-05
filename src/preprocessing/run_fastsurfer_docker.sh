3b1d3e2489a4fbc8be1d4d1e493638a161192de8
Ethan C <ethan.church2@mail.mcgill.com>
2026-04-05 09:30:57 -0400

    Changes to try snd fix surf

--- src/preprocessing/run_fastsurfer_docker.sh
+++ src/preprocessing/run_fastsurfer_docker.sh
@@ -85,10 +85,15 @@ process_subject() {
         fastsurfer_mode_args=()
     fi
 
+    # Memory limit for Docker: ~10GB should suffice for sequential hemisphere
+    # processing; increase to ~16GB+ if using --parallel.
+    local docker_mem_args=(--memory=10g --memory-swap=12g)
+
     echo "[$subject_name] Running FastSurfer Docker on $t1_for_fastsurfer"
     DOCKER_CONTENT_TRUST=0 docker run --rm -t \
         --user "$(id -u):$(id -g)" \
         "${docker_gpu_args[@]}" \
+        "${docker_mem_args[@]}" \
         -v "$subject_dir:/subject" \
         -v "$FS_LICENSE_PATH:/fs_license/license.txt:ro" \
         "$FASTSURFER_IMAGE" \
@@ -97,7 +102,6 @@ process_subject() {
         --sid "$subject_name" \
         --sd /subject/FastSurfer \
         --threads "$THREADS" \
-        --parallel \
         --no_hypothal \
         "${fastsurfer_mode_args[@]}"
 
